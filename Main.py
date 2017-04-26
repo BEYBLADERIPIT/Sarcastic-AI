21a63d3f3b8a128fc95a065379b63d7e461b0b1e
BEYBLADERIPIT <beybladeweeb@gmail.com>
2017-04-26 15:25:45 -0400

    Update Main.py
    ufygfhefg

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
 # Main python file
 
 # 
-sarcasticStatements = ["Hey, that was my button, can you PLEASE not do that!", "Other sarcastic statements here"]
+sarcasticStatements = ["Hey, that was my button, can you PLEASE not do that!", "ᴜᴘʟᴏᴀᴅ ᴄᴏᴍᴘʟᴇᴛᴇ.", "I will DDOS you if you click that again"]
 
 ifButtonOn = raw_input('0 or 1')
 if ifButtonOn = 0:
     print "Haha all done"
 else:
     print "You Failure. How DARE You!"
+if ifButtonOn = 1:
+    
+    
